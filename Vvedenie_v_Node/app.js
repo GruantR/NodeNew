6942bed321483aa4d1d55ee341cdce3da50c7b72
Ruslan_Trafimovich <ruworv@gmail.com>
2025-08-26 16:48:39 +0300

    Delete Vvedenie_v_Node directory

--- Vvedenie_v_Node/app.js
+++
@@ -1,5 +0,0 @@
-const mathFunctions = require('./math');
-console.log(mathFunctions.add(8, 2));      // Выведет: 10
-console.log(mathFunctions.multiply(8, 2)); // Выведет: 16
-console.log(mathFunctions.subtraction(8,2)); // Выведет 6
-console.log(mathFunctions.division(8,2)); //  Выведет 4
