8620387b979677812917f27c428fb8ea74ef518f
Ruslan_Trafimovich <ruworv@gmail.com>
2025-09-04 22:45:46 +0300

    go

--- First_server/src/config/db.js
+++ First_server/src/config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 require("dotenv").config();
 
-const MONGO_URL = `${process.env.MONGO_CONNECTION_STRING}/${process.env.MONGO_DB_NAME}`//Запуск БД на локальной машине
-//const MONGO_URL = `${process.env.MONGO_URL}` //Запуск БД на сервере Mongo
+//const MONGO_URL = `${process.env.MONGO_CONNECTION_STRING}/${process.env.MONGO_DB_NAME}`//Запуск БД на локальной машине
+const MONGO_URL = `${process.env.MONGO_URL}` //Запуск БД на сервере Mongo
 console.log('MONGO_URL:', MONGO_URL)
 
 async function connectDb () {
