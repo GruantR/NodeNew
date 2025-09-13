acdf06a9e1f576da60f8b60681fbb9c1f71aca68
Ruslan_Trafimovich <rusworv@gmail.com>
2025-09-13 21:39:43 +0300

    MONGO DB  full release

--- First_server/src/server.js
+++ First_server/src/server.js
@@ -28,24 +28,3 @@ app.listen(PORT, () => {
   console.log(`Запущен сервер на http://localhost:${PORT}`);
 });
 
-// Быстрый ввод BODY USER>
-// {
-//     "id":"1",
-//     "username": "Masha",
-//     "email":"ruslanusrulit@gmail.com",
-//     "password":"Qwe123"
-// }
-
-// Быстрый ввод BODY BOOKS>
-// {
-//     "id":"1",
-//     "bookname": "Masha i medved",
-//     "author":"Stepan",
-//     "pages":"123"
-// }
-// Быстрый ввод todos
-// {
-//     "title": "GoToSleep",
-//     "isCompleted":"false"
-// }
-// тест11111116666
