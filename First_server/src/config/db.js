7ad90989864eabcab6e702d95502034539eb349c
Ruslan_Trafimovich <rusworv@gmail.com>
2025-09-16 21:34:06 +0300

    Sequelize online

--- First_server/src/config/db.js
+++ First_server/src/config/db.js
@@ -1,22 +1,49 @@
-//db.js
-const Sequelize = require('sequelize')
+// //db.js
+// const Sequelize = require('sequelize')
+// require("dotenv").config();
+
+
+// const sequelize = new Sequelize(
+//   process.env.DATABASE,
+//   process.env.DB_USER,
+//   process.env.PASSWORD,
+//   {
+//     host: process.env.HOST,
+//     dialect: process.env.DIALECT,
+//     logging: (msg) => {
+//       // Выводим только SQL-запросы, игнорируем метаданные
+//       if (msg.includes('SELECT') || msg.includes('INSERT') || msg.includes('UPDATE') || msg.includes('DELETE')) {
+//         console.log(msg);
+//       }
+//     },
+//   }
+// );
+
+// module.exports = sequelize
+
+
+
+
+// db.js
+const Sequelize = require('sequelize');
 require("dotenv").config();
 
+// Создаем экземпляр Sequelize, используя полную строку подключения
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Это необходимо для подключения к Neon
+    }
+  },
+  logging: (msg) => {
+    // Выводим только SQL-запросы, игнорируем метаданные
+    if (msg.includes('SELECT') || msg.includes('INSERT') || msg.includes('UPDATE') || msg.includes('DELETE')) {
+      console.log(msg);
+    }
+  },
+});
 
-const sequelize = new Sequelize(
-  process.env.DATABASE,
-  process.env.DB_USER,
-  process.env.PASSWORD,
-  {
-    host: process.env.HOST,
-    dialect: process.env.DIALECT,
-    logging: (msg) => {
-      // Выводим только SQL-запросы, игнорируем метаданные
-      if (msg.includes('SELECT') || msg.includes('INSERT') || msg.includes('UPDATE') || msg.includes('DELETE')) {
-        console.log(msg);
-      }
-    },
-  }
-);
-
-module.exports = sequelize
+module.exports = sequelize;
--- First_server/src/server.js
+++ First_server/src/server.js
@@ -1,3 +1,4 @@
+//server.js
 const express = require("express");
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('../swaggerSpec.js'); 
